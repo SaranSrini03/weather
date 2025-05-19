bc43001e01f66c811c2374bb0237d98e7b4dfb3f
saransrini03 <saransrini003@gmail.com>
2025-05-19 20:14:56 +0530

    changed details

--- app/details/page.js
+++ app/details/page.js
@@ -1,11 +1,11 @@
-// app/details/page.jsx
+// app/details/page.jsx (Server Component)
 import { Suspense } from "react";
-import DetailsClient from "./DetailsClient";
+import DetailsClientWrapper from "./DetailsClientWrapper";
 
 export default function DetailsPage() {
   return (
-    <Suspense fallback={<div className="p-6">Loading weather details...</div>}>
-      <DetailsClient />
+    <Suspense fallback={<div className="p-6 text-white">Loading weather details...</div>}>
+      <DetailsClientWrapper />
     </Suspense>
   );
 }
